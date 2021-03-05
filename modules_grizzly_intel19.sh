add6e96cfef2f86ffb719d5065b69cf597364921
Mark Petersen <mpetersen@lanl.gov>
2021-03-05 06:09:27 -0700

    intel19

--- modules_grizzly_intel19.sh
+++ modules_grizzly_intel19.sh
@@ -5,11 +5,7 @@ module purge
 module use /usr/projects/climate/SHARED_CLIMATE/modulefiles/all/scorpio
 module load friendly-testing
 module load intel/19.0.4 intel-mpi/2019.4 hdf5-parallel/1.8.16 pnetcdf/1.11.2 netcdf-h5parallel/4.7.3 mkl/2019.0.4 pio2/1.10.1
-# note: if you already did this:
-#  module use /usr/projects/climate/SHARED_CLIMATE/modulefiles/all/
-# then it will show 'no such file' for hdf5-parallel/1.8.16. 
-# solution: log into a new node and try with only the commands above.
-export I_MPI_CC=icc
-export I_MPI_CXX=icpc
-export I_MPI_F77=ifort
-export I_MPI_F90=ifort
+
+module use /usr/projects/climate/SHARED_CLIMATE/modulefiles/all
+module unload python
+source /usr/projects/climate/SHARED_CLIMATE/anaconda_envs/load_latest_compass.sh
